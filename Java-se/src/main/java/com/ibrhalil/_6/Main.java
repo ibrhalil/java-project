9002b5858fff46fc2f46996fec7b2d7d9d46945c
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-09-09 17:28:11 +0300

    add wrapper and syntax

---
+++ Java-se/src/main/java/com/ibrhalil/_6/Main.java
@@ -0,0 +1,34 @@
+package com.ibrhalil._6;
+
+public class Main {
+    public static void main(String[] args) {
+        //package com.ibrhalil._6;
+        //ilgili java siniflarinin toplandigi paketler (com > ibrhalil > _6 > Main.java)
+
+        //public
+        //erişim belirleyici
+
+        //static
+        //yöntem nesne olmadan çağrılacaktır.
+
+        //void
+        //değer döndürmeyen metot belirleyici
+
+        //main
+        //java için özel bir metot
+
+        //String[] args
+        //String türünde argüman listesi kabul eder
+
+
+        //degisken veya metot tanimlamasi yaparken uyulmasi gerekenler
+        //([A-Z],[a-z],[0-9]), '$'(dolar işareti) ve '_' (alt çizgi) kullanilabilir.
+        //rakamla ([0-9]) baslanmaz.
+        //buyuk/kucuk (Aa) harfe duyarlidir.
+        //uzunlugu konusunda herhangi bir sinirlama yoktur
+        //ancak 4 – 15 harften olusan optimum uzunluk kullanılmasi tavsiye edilir.
+        //java da bazi ozel ifadeler var, bunlari degisken olarak kullamayiz
+        //int true = 5; true bir ozel ifadedir.
+
+    }
+}
---
+++ Java-se/src/main/java/com/ibrhalil/_6/Syntax.java
@@ -0,0 +1,34 @@
+package com.ibrhalil._6;
+
+public class Syntax {
+    public static void main(String[] args) {
+        //tek yorum satiri
+        /*
+            coklu yorum
+            satiri...
+
+         */
+
+        int number = 7;
+
+        {
+            String name = "Test";
+
+            System.out.println(name);
+            System.out.println(number);
+        }
+
+        //System.out.println(name);
+        System.out.println(number);
+
+
+    }
+}
+
+class test { //uygun olmayan sinif tanimlama
+
+}
+
+class Test { //uygun syntax
+
+}
