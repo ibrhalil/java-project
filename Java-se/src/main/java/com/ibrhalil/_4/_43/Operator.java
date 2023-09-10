85804e0db195f29d2da57da92c75796a61ef4c91
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-09-10 12:31:29 +0300

    java operatorler eklendi

---
+++ Java-se/src/main/java/com/ibrhalil/_4/_43/Operator.java
@@ -0,0 +1,17 @@
+package com.ibrhalil._4._43;
+
+public class Operator {
+    public static void main(String[] args) {
+        int a = 10;
+
+        a++;
+        a--;
+        ++a;
+
+        System.out.println("a : "+ a);
+        System.out.println("a++ : "+ a++);
+        System.out.println("a : "+ a);
+        System.out.println("++a : "+ ++a);
+
+    }
+}
