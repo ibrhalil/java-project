26b833f41f11e36b59cd946aca2152df45b6479f
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-09-16 11:27:28 +0300

    test commit
    (cherry picked from commit e04a5818109f7175327a6933b8635a2ede45dbda)
    
    (cherry picked from commit 64868e237e8096c07aca0a865ebe69333b615236)

--- java-se/src/main/java/com/ibrhalil/_0/_01/Main.java
+++ java-se/src/main/java/com/ibrhalil/_0/_01/Main.java
@@ -3,6 +3,7 @@ package com.ibrhalil._0._01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("test commit");
 
         System.out.println("test commit 2");
     }
