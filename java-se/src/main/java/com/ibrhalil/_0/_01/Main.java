e04a5818109f7175327a6933b8635a2ede45dbda
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-09-16 11:27:28 +0300

    test commit

--- java-se/src/main/java/com/ibrhalil/_0/_01/Main.java
+++ java-se/src/main/java/com/ibrhalil/_0/_01/Main.java
@@ -3,5 +3,6 @@ package com.ibrhalil._0._01;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        System.out.println("test commit");
     }
 }
