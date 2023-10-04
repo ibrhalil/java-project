004ceb991976fc79385a84246fd2b55cdbd5012e
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-10-04 16:46:48 +0300

    carpma islemi yapmadan carpma islemi yapma

---
+++ java-se/java-09-examples/src/main/java/com/ibrhalil/CarpmaIslemiKullanmadanCarpma.java
@@ -0,0 +1,13 @@
+package com.ibrhalil;
+
+public class CarpmaIslemiKullanmadanCarpma {
+    public static void main(String[] args) {
+        int num1 = 23;
+        int num2 = 12;
+        int sum = 0;
+        for (int i = 0; i < num2; i++) {
+            sum += num1;
+        }
+        System.out.println(num1 + " * " + num2 + " = " + sum);
+    }
+}
