a832719ccff351bb91b2b5fe3830fcc14084ebee
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-10-05 07:37:44 +0300

    char ornek

---
+++ java-se/java-01-variables/src/main/java/com/ibrhalil/_1primitive/_14/CharKullanim.java
@@ -0,0 +1,11 @@
+package com.ibrhalil._1primitive._14;
+
+public class CharKullanim {
+    public static void main(String[] args) {
+        char c;
+        for (int i = 0; i <= 256; i++) {
+            c = (char) i;
+            System.out.println(i + ".Char deger : " + "'" + c + "'");
+        }
+    }
+}
