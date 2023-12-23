ed2adda8d62f737a166299524b23ad6959635e16
Halil AYDIN <ibrhalilaydn@gmail.com>
2023-12-23 17:31:54 +0300

    ArrayBinarySearch

---
+++ java-se/java-09-examples/src/main/java/com/ibrhalil/ArrayBinarySearch.java
@@ -0,0 +1,11 @@
+package com.ibrhalil;
+
+import java.util.Arrays;
+
+public class ArrayBinarySearch {
+    public static void main(String[] args) {
+        char[] ch = {'f', 'g', 'a', 't', 'v', 'x', 'l'};
+
+        System.out.println(Arrays.binarySearch(ch, 't'));
+    }
+}
